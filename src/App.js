45471846b989d619e4492ed43400b1d37a55e7ac
greatyun <yjs8237@gmail.com>
2020-11-01 15:38:34 +0900

    최초 수정 커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
 
 function App() {
+  let name = "jisang";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {name === 'jisang' ? (<h1>{name} 안녕하세요1</h1>) : (<h1>안녕하세요2</h1>)}
+    </Fragment>
   );
 }
 
